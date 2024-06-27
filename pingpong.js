61f333e9cc6a7cbf9478202bf9b506b0992e390a
Sara Jenner <168777147+sarajenner@users.noreply.github.com>
2024-06-27 17:30:01 -0300

    atualiz.ação de cor

--- pingpong.js
+++ pingpong.js
@@ -1,10 +1,10 @@
 function setup(){
     //aqui vou criar minha "mesa"
-    createCanva (600,400);
+    createCanvas (600,400);
 }
 
 function draw (){
     //aqui vou por a cor da "masa"
     //A cor da mesa é em rgb
-    backgroud (230,230,250);
+    background (230,230,250);
 }
