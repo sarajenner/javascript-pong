e72172f1525cdba96d9572c7a21ba15a4c755203
Sara Jenner <168777147+sarajenner@users.noreply.github.com>
2024-08-01 16:41:55 -0300

    saraburra

--- pingpong.js
+++ pingpong.js
@@ -1,10 +1,79 @@
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+let raioBolinha = 25;
+
+//config velocidade bolinha
+let xvelocidadeBolinha = 6;
+let yvelocidadeBolinha = 6;
+
+//configura raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let larguraRaquete = 10;
+let alturaRaquete = 90;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
 }
 
 function draw(){
-    //aqui vou por a cor da "masa"
+    //função responsável pelo "desenho e animação da mesa"
+    //aqui vou por a cor da "mesa"
     //A cor da mesa é em rgb
-    background (123,104,238);
+    background(10,10,10); 
+    //chamando a função cria bolinha para criar a bolinha
+    criaBolinha(xBolinha, yBolinha, tamBolinha);
+    //chamando a função mov bolinha
+    moveBolinha();
+    //chamando a borda
+    Borda();
+    criaRaquete(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
+    movimentaRaquete();
+    colideRaquete();
+}
+
+//função bolinha
+function criaBolinha(xBolinha, yBolinha,tamBolinha){
+    circle (xBolinha,yBolinha,tamBolinha);
+}
+
+//função move bolinha
+function moveBolinha(){
+    xBolinha = xvelocidadeBolinha + xBolinha;
+    yBolinha = yvelocidadeBolinha + yBolinha;
+}
+
+function Borda(){
+    if (xBolinha > width || xBolinha < 0){
+        xvelocidadeBolinha *= -1;
+    }
+    if (yBolinha > height || yBolinha < 0){
+        yvelocidadeBolinha *= -1;
+    }
+}
+
+//função responsável por cirar o retângulo que representa a raquete
+function criaRaquete(xRaquete, yRaquete, larguraRaquete, alturaRaquete){
+    fill("blue");
+    rect(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
+}
+//função reponsável por movimentar a raquete
+function movimentaRaquete() {
+    if(keyIsDown(UP_ARROW)){
+        yRaquete -= 10;
+    }
+    if(keyIsDown(DOWN_ARROW)){
+        yRaquete += 10;
+    }
+}
+//função responsável por quando a bolinha bater na raquete, retornar em diração contrária.
+function colideRaquete(){
+    if(xBolinha - raioBolinha < xRaquete + larguraRaquete && yBolinha - raioBolinha < yRaquete + alturaRaquete && yBolinha + raioBolinha > yRaquete){
+        
+        xvelocidadeBolinha *= -1;
+        
+    }
 }
